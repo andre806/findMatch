d3f0ee0b331b3fed07259d21b37ae9345e0fc7fb
andre806 <megagamescanal6@gmail.com>
2025-10-24 20:20:00 -0300

    Initial commit - Spring Boot Gothic Match backend

---
+++ src/main/java/com/example/demo/GothicMatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GothicMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GothicMatchApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,40 @@
+package com.example.demo.models;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Document(collection = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    private String id;
+    private String nome;
+    private Integer idade;
+    private String senha;
+    private String email;
+    private String spotifyId; 
+    private String gostoMusical;
+    private List<String> gostos;
+    private List<String> urlFotos;
+    private String cidade;
+    private String genero;
+    private String sexualidade;
+    private String interesse;
+    private String toProcurando;
+    private String bio;
+    private List<String> cidadesExibicao; 
+    private String numeroTelefone; 
+    private String ocupacao;
+    private String educacao;
+    private String urlFotoPerfil; 
+    private boolean premium;
+}
